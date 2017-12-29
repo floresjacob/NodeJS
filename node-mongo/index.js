076a500daf3ce2eb27f5aeaf0f265836eeeb29fd
Jacob Flores <jacob.e.flores562@gmail.com>
2017-12-29 14:51:03 -0800

    Node Callback Hell and Promises

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -4,11 +4,11 @@ const dboper = require('./operations');
 const url = 'mongodb://localhost:27017/conFusion';
 
 
-const writeDishes = function(db, callback){
-    const collection = db.collection("dishes");
-
-
-    //Example 1; with .collection for Mongo 3.x
+// const writeDishes = function(db, callback){
+//     const collection = db.collection("dishes");
+//
+//
+//     Example 1; with .collection for Mongo 3.x
     // collection.insertOne({"name": "Uthappizza", "description": "test_0"},
     // (err, result) => {
     //     assert.equal(err,null);
@@ -28,38 +28,43 @@ const writeDishes = function(db, callback){
     //         callback;
     //     });
     // });
+//
+//   }
 
-      dboper.insertDocument(db, { name: "Vadonut", description: "Test"},
-        "dishes", (result) => {
-            console.log("Insert Document:\n", result.ops);
-
-            dboper.findDocuments(db, "dishes", (docs) => {
-                console.log("Found Documents:\n", docs);
-
-                dboper.updateDocument(db, { name: "Vadonut" },
-                    { description: "Updated Test" }, "dishes",
-                    (result) => {
-                        console.log("Updated Document:\n", result.result);
-
-                        dboper.findDocuments(db, "dishes", (docs) => {
-                            console.log("Found Updated Documents:\n", docs);
-
-                            db.dropCollection("dishes", (result) => {
-                                console.log("Dropped Collection: ", result);
-                            });
-                        });
-                      });
-                    });
-                  });
-                }
-
-MongoClient.connect(url, (err, client) => {
+MongoClient.connect(url, function(err, client){
 
     assert.equal(err,null);
 
-    console.log('Connected correctly to server');
+    const db = client.db('conFusion');
 
-    writeDishes(client.db('conFusion'), function(){
-      db.close();
-    });
-});
+    console.log('Connected correctly to server');
+    dboper.insertDocument(db, { name: "Vadonut", description: "Test" },
+        "dishes")
+        .then((result) => {
+            console.log("Insert Document:\n", result.ops);
+
+            return dboper.findDocuments(db, "dishes");
+        })
+        .then((docs) => {
+            console.log("Found Documents:\n", docs);
+
+            return dboper.updateDocument(db, { name: "Vadonut" },
+                { description: "Updated Test" }, "dishes");
+        })
+        .then((result) => {
+            console.log("Updated Document:\n", result.result);
+
+            return dboper.findDocuments(db, "dishes");
+        })
+        .then((docs) => {
+            console.log("Found Updated Documents:\n", docs);
+
+            return db.dropCollection("dishes");
+        })
+        .then((result) => {
+            console.log("Dropped Collection: ", result);
+
+            return client.close();
+        })
+        .catch((err) => console.log(err));
+      });
