827f03eb09e8f599cfad7c15fa3046eea7ecdfb5
Jacob Flores <jacob.e.flores562@gmail.com>
2017-12-30 12:33:49 -0800

    Mongoose pt 3

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -4,7 +4,7 @@ mongoose.Promise = require('bluebird');
 const Dishes = require('./models/dishes');
 
 const url = 'mongodb://localhost:27017/conFusion';
-const connect = mongoose.createConnection(url, {
+const connect = mongoose.connect(url, {
     useMongoClient: true
 });
 
@@ -17,22 +17,41 @@ connect.then((db) => {
         description: 'test'
     });
 
-    newDish.save()
-        .then((dish) => {
-            console.log(dish);
+    Dishes.create({
+          name: 'Uthappizza',
+          description: 'test'
+      })
+          .then((dish) => {
+              console.log(dish);
 
-            return Dishes.find({}).exec();
-        })
-        .then((dishes) => {
-            console.log(dishes);
+              return Dishes.findByIdAndUpdate(dish._id, {
+                  $set: { description: 'Updated test' }
+              }, {
+                      new: true
+                  })
+                  .exec();
+          })
+          .then((dish) => {
+              console.log(dish);
 
-            return db.collection('dishes').drop();
-        })
-        .then(() => {
-            return db.close();
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+              dish.comments.push({
+                  rating: 5,
+                  comment: 'I\'m getting a sinking feeling!',
+                  author: 'Leonardo di Carpaccio'
+              });
+
+              return dish.save();
+          })
+          .then((dish) => {
+              console.log(dish);
+
+              return db.collection('dishes').drop();
+          })
+          .then(() => {
+              return db.close();
+          })
+          .catch((err) => {
+              console.log(err);
+          });
 
 });
